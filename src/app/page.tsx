c45d02818242955f2dc01d126023805e3db57aaf
Aditya Khandelwal <f20230452@pilani.bits-pilani.ac.in>
2024-03-21 22:23:15 +0530

    adding files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,59 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={styles.main}>
+      <div className={styles.wrapper}>
+      <div className={styles.attentionDiv}>
+        <div className={styles.attentionText}>
+          <div>
+            Attention, <span className={styles.blueText}>BITSIANs</span>
+          </div>
+          <p>
+            If you're eager to enhance your abilities and boost your chances of securing a fantastic SI or placement opportunity, you're in the right place! Gain access to <span className={styles.blueText}>Placement Training Resources</span> right now and build your resume using Blindfold AI's own <span className={styles.blueText}>AI-powered resume builder</span>!
+          </p>
+          <div>
+            It's time to take charge of your future and soar to new heights!🚀
+          </div>
+        </div>
+
+
+      </div>
+      <div className={styles.middleDivsWrapper}>
+        <div className={styles.middleDiv}>
+          <div className={styles.middleDivHeader}>
+            <h3><span className={styles.pinkText}>AI-powered</span> Resume Builder</h3>
+          </div>
+          <div className={styles.middleDivText}>
+            Level up your resume with <span className={styles.pinkText}>Blindfold.ai</span> Resumes!
+            Enhance your resume with Al curated content, easily manage multiple resumes for different target jobs and create biased masked ATS-friendly resumes....
+          </div>
+          <div className={styles.btnWrapper}>
+            <div className={styles.middleDivBtn}>
+              Start Building With Blindfold.ai
+            </div>
+          </div>
+        </div>
+        <div className={styles.middleDiv}>
+          <div className={styles.middleDivHeader}>
+            <h3><span className={styles.pinkText}>One-Click Apply</span> to Jobs</h3>
+          </div>
+          <div className={styles.middleDivText}>
+            Using <span className={styles.pinkText}>Blindfold.ai's Chrome extension</span>  you can one click fill out job applications using your preferred resume, making applying to jobs on platforms like Greenhouse, Lever, Linkedin, and Eightfold.ai a breeze.
+          </div>
+          <div className={styles.btnWrapper}>
+            <div className={styles.middleDivBtn}>
+              Download Chrome Extension
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div>
+        <h3>
+          Resume Resources
+        </h3>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
-    </main>
-  );
+    </div>)
 }
